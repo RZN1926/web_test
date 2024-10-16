f7a26731bfd2e2ea228ec6b574912d355f7e09d6
Daniel <dan.email@example.com>
2024-10-16 15:07:39 +0600

    I add data base in render

--- django_project/settings.py
+++ django_project/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'blogdb_fp5r',
+        'USER': 'user',
+        'PASSWORD': 'VgU0i0DZYoUONQoRaJSxmXOhdqrEwWu0',
+        'HOST': 'dpg-cs7nc4bv2p9s73f622h0-a.singapore-postgres.render.com',
+        'PORT': '5432'
     }
 }
 
@@ -121,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 # Default primary key field type
